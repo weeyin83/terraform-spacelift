8460ddb4ee4dbe9bdc342d0c46dc78d45a1a3676
Sarah Lean <Sarah.Lean@microsoft.com>
2024-02-12 11:23:16 +0000

    update info

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ resource "azurerm_resource_group" "resource_group_name" {
 }
 
 resource "azurerm_storage_account" "learning" {
-    name = "techielassterraformdeploy"
+    name = "tlassstorage"
     resource_group_name = azurerm_resource_group.resource_group_name
     location = var.location
-    account_tier = "standard"
+    account_tier = "Standard"
     account_replication_type = "GRS"
 
     tags = {
